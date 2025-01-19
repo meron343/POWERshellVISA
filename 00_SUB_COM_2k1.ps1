a480a5aa02c02e28d480a9bae8a3603551d3f3fc
meron343 <dcfvgb-b@yahoo.co.jp>
2025-01-20 00:25:36 +0900

    Update 00_SUB_COM_2k1.ps1

--- 00_SUB_COM_2k1.ps1
+++ 00_SUB_COM_2k1.ps1
@@ -94,9 +94,111 @@ function SND_CMD($str){
         	return -1
 	}	
 }
+
+function QUE_CMD($str){
+	try{
+        	$Buffers = $str.split("[, :]",3)
+        	$INDEX= $Buffers[0]
+        	$QorS= $Buffers[1]
+        	$COMAND= $Buffers[2]
+    	}catch{
+        	write-host "入力形式が違います。ex:PWR,Q,VOLT?"
+        	return -1
+	}
+	try{
+	    	if( $hashtable[$INDEX].Dev -like "SKT" ){
+
+		    	$hashtable[$INDEX].writer.WriteLine(   $COMAND   )
+		    	$hashtable[$INDEX].writer.Flush()
+			#write-host 	$hashtable[$INDEX].ip send $COMAND
+			if( $QorS[0] -like "S" ){ return }
+			
+
+			#受信中は待つ(500MS
+			while($hashtable[$INDEX].stream.DataAvailable -ne $true){
+				start-sleep -m 500
+				write-host -NoNewline　"RCV待`r"
+			}
+			write-host -NoNewline　"          `r"
+
+			while($hashtable[$INDEX].stream.DataAvailable){
+				$read = $hashtable[$INDEX].stream.Read($hashtable[$INDEX].buffer, 0, 1024)
+				$ascii_data = $hashtable[$INDEX].encoding.GetString($hashtable[$INDEX].buffer , 0 , $read)
+				$hashtable[$INDEX].STR= $ascii_data
+
+			}
+			$hashtable[$INDEX].BIN= $hashtable[$INDEX].buffer
+			if( $QorS[1] -like "C" ){
+				write-host $ascii_data
+			}else{
+			}
+			#return 1
+
+		 }
+		if( $hashtable[$INDEX].Dev -like "COM" ){
+			$hashtable[$INDEX].compt.WriteLine($COMAND) 
+			Start-Sleep -Milliseconds 50
+			if( $QorS[0] -like "S" ){ return }
+			
+			$date = get-date		#現在時刻取得
+			$dateend = $date.Addseconds(+2) #現在時刻取得+2
+
+			while($date -le $dateend){
+				$date = get-date
+				if ($hashtable[$INDEX].compt.BytesToRead -gt 0){
+					$srdt = $hashtable[$INDEX].compt.ReadByte()
+					$hashtable[$INDEX].brcv[       $hashtable[$INDEX].ircvLen     ] = $srdt
+					$hashtable[$INDEX].ircvLen++
+
+					if ($srdt -eq 0x0a){  #LF
+				        	$sr = [System.Text.Encoding]::ASCII.GetString($hashtable[$INDEX].brcv, 0, $hashtable[$INDEX].ircvLen)
+				        	#Write-Host $sr
+				        	$hashtable[$INDEX].brcv[  $hashtable[$INDEX].ircvLen  ] = 0x0a #LF
+				        	$hashtable[$INDEX].ircvLen++
+				        	#$compt.Write($brcv, 0, $ircvLen[$dev_sel_com])
+				        	$hashtable[$INDEX].ircvLen = 0
+						$dateend = $date
+				      	}
+
+
+					if ($srdt -eq 0x0d){  #CR
+				        	$sr = [System.Text.Encoding]::ASCII.GetString($hashtable[$INDEX].brcv, 0, $hashtable[$INDEX].ircvLen)
+				        	#Write-Host $sr
+				        	$hashtable[$INDEX].brcv[  $hashtable[$INDEX].ircvLen  ] = 0x0a #LF
+				        	$hashtable[$INDEX].ircvLen++
+				        	#$compt.Write($brcv, 0, $ircvLen[$dev_sel_com])
+				        	$hashtable[$INDEX].ircvLen = 0
+						$dateend = $date
+				      	}
+
+
+				}
+			}
+
+			$hashtable[$INDEX].STR= $sr
+
+			if( $QorS[1] -like "C" ){
+				write-host $sr
+			}else{
+			}
+
+
+
+	    	}
+	}catch{
+		write-host "ERR"
+        	return -1
+	}	
+}
+
 <#　EXLIST
 OPN_DEV "PWR SKT 192.168.1.2:5025"
 OPN_DEV "RERAY COM COM9:9600"
+
+QUE_CMD "PWR,S,VOLT 12" SEND_ONLY
+QUE_CMD "PWR,QC,*IDN?"  write-host
+QUE_CMD "PWR,Q,*IDN?"	NON write-host
+
 SND_CMD "PWR,S,VOLT 12"
 SND_CMD "RERAY,S,DO1 1"
 sleep 3
